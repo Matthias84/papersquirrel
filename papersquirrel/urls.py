a96cfe66c1739f59d6b4258c937e9291ff04b532
matthias <mame@plattform32.de>
2019-06-02 15:02:42 +0200

    Add dummy view

--- papersquirrel/urls.py
+++ papersquirrel/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from squirrel import views
+
+app_name = 'squirrel'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('squirrel/', include('squirrel.urls')),
 ]
+
+# /articles
+# /articles/new
+# /article/
+# /profile/
+# /options/
+
