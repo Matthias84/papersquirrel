429c36e9a394b3c504e2300433782c3388895ecf
matthias <mame@plattform32.de>
2019-06-02 18:35:06 +0200

    Switch Generic Index view

--- squirrel/views.py
+++ squirrel/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Article
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    template_name = 'squirrel/index.html'
+    context_object_name = 'latest_article_list'
+    
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Article.objects.order_by('-download_date')[:5]
