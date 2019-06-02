8e6d3ac34c6c2e9afa1e4303e2e43382f3598926
matthias <mame@plattform32.de>
2019-06-02 11:33:03 +0200

    Add article

--- papersquirrel/settings.py
+++ papersquirrel/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'squirrel.apps.SquirrelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- squirrel/models.py
+++ squirrel/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.CharField(max_length=500)
+    teaser_text = models.TextField(max_length=500, editable = False)
+    pub_date = models.DateTimeField('date published')
+    download_date = models.DateTimeField('date downloaded')
+    author = models.CharField(max_length=250)
+    publisher = models.CharField(max_length=250)
+    copyright = models.CharField(max_length=250)
+    download_url = models.URLField()
+    source_html = models.TextField()
+    source_markdown = models.TextField()
+    wordcount = models.BigIntegerField()
+    # TODO: readingtime, keywords, previewimage, language
+    # TODO: isRead, isStarred
