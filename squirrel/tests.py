09c6eab45a872a128b52d32081f01a4b1965f614
matthias <mame@plattform32.de>
2019-08-10 11:10:20 +0200

    Fix old tests

--- squirrel/tests.py
+++ squirrel/tests.py
@@ -14,18 +14,18 @@ class ArticleModelTests(TestCase):
         """
         Minimal create and fetch article scenario
         """
-        article = Article.add('https://localhost',html_source='<html></html>')
+        article = Article.add('https://localhost','' ,html_source='<html></html>')
         self.assertEqual(article.id, 1)
         self.assertTrue(timezone.now() - datetime.timedelta(seconds=30) < article.download_date < timezone.now())
         self.assertEqual(article.source_html, '<html></html>')
 
     def test_add_empty(self):
-        article = Article.add('https://localhost',html_source='<html></html>')
+        article = Article.add('https://localhost','' , html_source='<html></html>')
         self.assertEqual(article.id, 1)
         self.assertEqual(article.source_html, '<html></html>')
 
     def test_add_content_analysis(self):
         with codecs.open('./squirrel/tests/basic-html.html', 'r', 'utf-8') as f:
             src = f.read()
-            article = Article.add('https://localhost/basic-html/',html_source=src)
+            article = Article.add('https://localhost/basic-html/', '', html_source=src)
             self.assertEqual(article.source_html, src)
